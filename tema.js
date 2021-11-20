c6c02cd9832860470dfdf8cd022d751a8743ca24
camila <mccotazo9@misena.edu.co>
2021-11-20 13:12:45 -0500

    repo01

---
+++ tema.js
@@ -0,0 +1,24 @@
+
+class Artista {
+    constructor(nombre) {
+        this._nombre = nombre;
+    }
+}
+class Genero {
+    constructor(tipo_cancion) {
+        this._tipo_cancion = tipo_cancion;
+    }
+}
+class Album {
+    constructor(cancion, _fecha_lanzamiento) {
+        this._cancion = cancion;
+        this._fecha_lanzamiento = fecha_lanzamiento;
+    }
+}
+class Cancion{
+    constructor(tipo_artista){
+        this._tipo_artista = tipo_artista;
+    }
+    setTipo_artista
+} 
+
